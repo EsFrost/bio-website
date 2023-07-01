c8984e82f08500bd4375cc0f625cad236a018375
Sigmund Frost <sigmafi.tech@gmail.com>
2023-07-01 17:50:44 +0300

    Update Navbar.jsx
    Fixed annoying bug with hiding navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,11 @@ import { v4 as uuidv4 } from 'uuid';
 const Navbar = () => {
 
     const [isClicked, setIsClicked] = useState(false)
+
     const ref = useOnclickOutside(() => {
-        setIsClicked(!isClicked)
+        if (isClicked) {
+            setIsClicked(!isClicked)
+        }
     })
 
 
@@ -16,11 +19,11 @@ const Navbar = () => {
         <>
             <nav className="navbar-items" id="test">
                 <h1>SigmaFi <FaReact className='logo-icon' /></h1>
-                <div className="menu-icons">
+                <div ref={ref} className="menu-icons">
                     <FaBars className={!isClicked ? 'menu-icons' : 'hidden'} onClick={() => setIsClicked(!isClicked)} />
                     <FaTimes className={isClicked ? 'menu-icons' : 'hidden'} onClick={() => setIsClicked(!isClicked)} />
                 </div>
-                <ul ref={ref} className={isClicked ? 'active' : ''}>
+                <ul className={isClicked ? 'active' : ''} ref={ref} >
                     <li key={uuidv4()}><a onClick={() => setIsClicked(!isClicked)} href="#bio" className='nav-links'><FaUserAlt className='nav-list-icons' />Bio</a></li>
                     <li key={uuidv4()}><a onClick={() => setIsClicked(!isClicked)} href="#skills" className='nav-links'><FaRegAddressCard className='nav-list-icons' />Skills</a></li>
                     <li key={uuidv4()}><a onClick={() => setIsClicked(!isClicked)} href="#contact" className='nav-links'><FaAddressBook className='nav-list-icons' />Contact</a></li>
